e6e09a990b69ff15281a6a49c83754fedcce9d2f
Dmytro Piliugin <dmytro.piliugin@gmail.com>
2014-04-10 13:03:03 +0300

    move Ability model to support folder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,3 +27,6 @@ end
   end
 end
 
+# Add support to load paths
+$:.unshift File.expand_path('../support', __FILE__)
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
---
+++ spec/support/ability.rb
@@ -0,0 +1,7 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+  end
+end
+
