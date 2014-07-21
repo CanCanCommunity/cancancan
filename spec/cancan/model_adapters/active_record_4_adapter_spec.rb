b588170042fd4b4920b5d92b740642f33af04a5f
Bryan Rite <bryan@bryanrite.com>
2014-07-21 21:40:40 +0200

    Fix specs from colliding with each other. This is not the best solution, but too much work needs to be done to isolate these specs properly so I’ll delegate that to a different PR. In the meantime this keeps them separate and properly tests the functionality.

--- spec/cancan/model_adapters/active_record_4_adapter_spec.rb
+++ spec/cancan/model_adapters/active_record_4_adapter_spec.rb
@@ -6,34 +6,35 @@ if defined? CanCan::ModelAdapters::ActiveRecord4Adapter
       ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
       ActiveRecord::Migration.verbose = false
       ActiveRecord::Schema.define do
-        create_table(:users) do |t|
+        create_table(:parents) do |t|
           t.timestamps
         end
 
-        create_table(:articles) do |t|
+        create_table(:children) do |t|
           t.timestamps
-          t.integer :user_id
+          t.integer :parent_id
         end
       end
+
+      class Parent < ActiveRecord::Base
+        has_many :children, lambda { order(:id => :desc) }
+      end
+
+      class Child < ActiveRecord::Base
+        belongs_to :parent
+      end
+
       (@ability = double).extend(CanCan::Ability)
     end
 
     it "respects scope on included associations" do
-      class User < ActiveRecord::Base
-        has_many :articles, lambda { order("articles.id DESC") }
-      end
+      @ability.can :read, [Parent, Child]
 
-      class Article < ActiveRecord::Base
-        belongs_to :user
-      end
+      parent = Parent.create!
+      child1 = Child.create!(:parent => parent, :created_at => 1.hours.ago)
+      child2 = Child.create!(:parent => parent, :created_at => 2.hours.ago)
 
-      @ability.can :read, [User, Article]
-
-      user = User.create!
-      article1 = Article.create!(:user => user, :created_at => 1.hours.ago)
-      article2 = Article.create!(:user => user, :created_at => 2.hours.ago)
-
-      expect(User.accessible_by(@ability).order('users.created_at ASC').includes(:articles).first.articles).to eq [article2, article1]
+      expect(Parent.accessible_by(@ability).order(:created_at => :asc).includes(:children).first.children).to eq [child2, child1]
     end
   end
 end
