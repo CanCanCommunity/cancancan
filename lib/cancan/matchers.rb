c1a999778e80c2bd8e5390aee784e25b993792c8
Jon Kinney <jonkinney@gmail.com>
2014-12-14 12:37:43 -0600

    Shorten when success, detail when fail
    Previously I was only doing the short version, then I went to all
    detailed versions when the object was not an instance_of?(Class) but
    when it succeeds we don't need to see those details. If you watch it
    fail first then you can see the details for the resource, then when it
    passes those details can be hidden. They are always exposed for the
    failure case though.

--- lib/cancan/matchers.rb
+++ lib/cancan/matchers.rb
@@ -38,11 +38,6 @@ Kernel.const_get(rspec_module)::Matchers.define :be_able_to do |*args|
   end
 
   description do
-    resource = args[1]
-    if resource.instance_of?(Class)
-      "expected to be able to #{args.map(&:to_s).join(' ')}"
-    else
-      "expected to be able to #{args.map(&:inspect).join(' ')}"
-    end
+    "expected to be able to #{args.map(&:to_s).join(' ')}"
   end
 end
