d20563f4e7825b61aa2ca80b27a25989f8d66f46
Marnen Laibow-Koser <marnen@marnen.org>
2015-03-16 13:38:38 -0400

    Extract context.

--- spec/cancan/model_adapters/active_record_4_adapter_spec.rb
+++ spec/cancan/model_adapters/active_record_4_adapter_spec.rb
@@ -6,36 +6,40 @@ if defined? CanCan::ModelAdapters::ActiveRecord4Adapter
       before :each do
         ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
         ActiveRecord::Migration.verbose = false
-        ActiveRecord::Schema.define do
-          create_table(:parents) do |t|
-            t.timestamps :null => false
-          end
-
-          create_table(:children) do |t|
-            t.timestamps :null => false
-            t.integer :parent_id
-          end
-        end
-
-        class Parent < ActiveRecord::Base
-          has_many :children, lambda { order(:id => :desc) }
-        end
-
-        class Child < ActiveRecord::Base
-          belongs_to :parent
-        end
-
         (@ability = double).extend(CanCan::Ability)
       end
 
-      it "respects scope on included associations" do
-        @ability.can :read, [Parent, Child]
+      context 'associations' do
+        before :each do
+          ActiveRecord::Schema.define do
+            create_table(:parents) do |t|
+              t.timestamps :null => false
+            end
 
-        parent = Parent.create!
-        child1 = Child.create!(:parent => parent, :created_at => 1.hours.ago)
-        child2 = Child.create!(:parent => parent, :created_at => 2.hours.ago)
+            create_table(:children) do |t|
+              t.timestamps :null => false
+              t.integer :parent_id
+            end
+          end
 
-        expect(Parent.accessible_by(@ability).order(:created_at => :asc).includes(:children).first.children).to eq [child2, child1]
+          class Parent < ActiveRecord::Base
+            has_many :children, lambda { order(:id => :desc) }
+          end
+
+          class Child < ActiveRecord::Base
+            belongs_to :parent
+          end
+        end
+
+        it "respects scope on included associations" do
+          @ability.can :read, [Parent, Child]
+
+          parent = Parent.create!
+          child1 = Child.create!(:parent => parent, :created_at => 1.hours.ago)
+          child2 = Child.create!(:parent => parent, :created_at => 2.hours.ago)
+
+          expect(Parent.accessible_by(@ability).order(:created_at => :asc).includes(:children).first.children).to eq [child2, child1]
+        end
       end
     end
 
