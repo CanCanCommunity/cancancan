54f307da46747c04561e555b40c6912b3fde6a48
nyamadori <nyamadorig@gmail.com>
2017-11-21 18:39:39 +0900

    Support translation of model_name
    Add spec example
    
    Add translations in another language
    
    Use I18n#with_locale instead of I18n#default_locale=

--- lib/cancan/ability.rb
+++ lib/cancan/ability.rb
@@ -178,9 +178,18 @@ module CanCan
     def unauthorized_message(action, subject)
       keys = unauthorized_message_keys(action, subject)
       variables = { action: action.to_s }
-      variables[:subject] = (subject.class == Class ? subject : subject.class).to_s.underscore.humanize.downcase
+      variables[:subject] = translate_subject(subject)
       message = I18n.translate(keys.shift, variables.merge(scope: :unauthorized, default: keys + ['']))
       message.blank? ? nil : message
+    end
+
+    def translate_subject(subject)
+      klass = (subject.class == Class ? subject : subject.class)
+      if klass.respond_to?(:model_name)
+        klass.model_name.human
+      else
+        klass.to_s.underscore.humanize.downcase
+      end
     end
 
     def attributes_for(action, subject)
