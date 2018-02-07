4fff3253768e06dcca5e09297f365e0577d7f7fa
Tad Thorley <phaedryx@gmail.com>
2018-02-07 09:07:44 -0700

    please rubocop

--- lib/cancan/ability/strong_parameter_support.rb
+++ lib/cancan/ability/strong_parameter_support.rb
@@ -4,17 +4,21 @@ module CanCan
       # Return an array of attributes suitable for use with strong parameters
       def permitted_attributes(action, subject)
         @permitted_attributes ||= {}
-        @permitted_attributes[[action, subject]] ||= begin
-          attributes = relevant_rules(action, subject).reduce([]) do |array, rule|
-            if rule.attributes.empty? && subject.class == Class # empty attributes is an 'all'
-              array + subject.instance_methods.map(&:to_sym)
-            else
-              array + rule.attributes
-            end
+        @permitted_attributes[[action, subject]] ||= allowed_attributes(action, subject)
+      end
+
+      private
+
+      def allowed_attributes(action, subject)
+        attributes = relevant_rules(action, subject).reduce([]) do |array, rule|
+          if rule.attributes.empty? && subject.class == Class # empty attributes is an 'all'
+            array + subject.instance_methods.map(&:to_sym)
+          else
+            array + rule.attributes
           end
-          attributes.uniq!
-          attributes.select { |attribute| can?(action, subject, attribute) }
         end
+        attributes.uniq!
+        attributes.select { |attribute| can?(action, subject, attribute) }
       end
     end
   end
