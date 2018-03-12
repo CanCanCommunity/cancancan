3a3306cff64c0da4b108e90543b4041722d6ed1c
Ryan Krafnick <kraflab@gmail.com>
2018-03-12 19:21:09 +0100

    Include conditions in AccessDenied exception (#482)
    Include conditions in AccessDenied exception

--- lib/cancan/exceptions.rb
+++ lib/cancan/exceptions.rb
@@ -33,13 +33,14 @@ module CanCan
   # See ControllerAdditions#authorized! for more information on rescuing from this exception
   # and customizing the message using I18n.
   class AccessDenied < Error
-    attr_reader :action, :subject
+    attr_reader :action, :subject, :conditions
     attr_writer :default_message
 
-    def initialize(message = nil, action = nil, subject = nil)
+    def initialize(message = nil, action = nil, subject = nil, conditions = nil)
       @message = message
       @action = action
       @subject = subject
+      @conditions = conditions
       @default_message = I18n.t(:"unauthorized.default", default: 'You are not authorized to access this page.')
     end
 
