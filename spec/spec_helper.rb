c274523c587a6a70a5f1837cb86d8e77d6c1ff9f
Alessandro Rodi <alessandro.rodi@renuo.ch>
2018-09-16 15:50:34 +0200

    Improve CHANGELOG formatting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,7 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 RSpec.configure do |config|
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
+  config.disable_monkey_patching = true
   config.mock_with :rspec
   config.order = 'random'
 
