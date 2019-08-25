5240d136226b134cb57c932fd75a4a5c12700a04
Alex Ghiculescu <alex@tanda.co>
2019-08-25 17:21:30 -0500

    `accessible_by` may crash when a `default_scope` is used
    If you have a `default_scope` that sets ordering, and define a permission that checks for records via multiple tables, and use a `has_many :through`, and run Postgres... then on version 3.x, `accessible_by` may crash. The attached test suite shows how; the tests that call `alex.blog_post_comments` fail with:
    
    ```
         ActiveRecord::StatementInvalid:
           PG::InvalidColumnReference: ERROR:  for SELECT DISTINCT, ORDER BY expressions must appear in select list
           LINE 1: ... ORDER BY "blog_post_comments"."created_at" DESC, "blog_post...
                                                                        ^
           : SELECT DISTINCT "blog_post_comments".* FROM "blog_post_comments" INNER JOIN "blog_posts" ON "blog_post_comments"."blog_post_id" = "blog_posts"."id" LEFT OUTER JOIN "blog_posts" "blog_posts_blog_post_comments" ON "blog_posts_blog_post_comments"."id" = "blog_post_comments"."blog_post_id" LEFT OUTER JOIN "blog_authors" ON "blog_authors"."id" = "blog_posts_blog_post_comments"."blog_author_id" WHERE "blog_posts"."blog_author_id" = $1 AND "blog_authors"."id" = $2 ORDER BY "blog_post_comments"."created_at" DESC, "blog_posts"."title" ASC
    ```
    
    This regressed in https://github.com/CanCanCommunity/cancancan/pull/512

---
+++ spec/cancan/model_adapters/active_record_5_adapter_with_default_scopes_spec.rb
@@ -0,0 +1,146 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+if CanCan::ModelAdapters::ActiveRecordAdapter.version_greater_or_equal?('5.0.0')
+  describe CanCan::ModelAdapters::ActiveRecord5Adapter do
+    context 'with postgresql' do
+      before :each do
+        connect_db
+        ActiveRecord::Migration.verbose = false
+
+        ActiveRecord::Schema.define do
+          create_table(:blog_authors) do |t|
+            t.string :name, null: false
+            t.timestamps null: false
+          end
+
+          create_table(:blog_posts) do |t|
+            t.references :blog_author
+            t.string :title, null: false
+            t.timestamps null: false
+          end
+
+          create_table(:blog_post_comments) do |t|
+            t.references :blog_post
+            t.string :body, null: false
+            t.timestamps null: false
+          end
+        end
+
+        unless defined?(BlogAuthor)
+          class BlogAuthor < ActiveRecord::Base
+            has_many :blog_posts
+            has_many :blog_post_comments, through: :blog_posts
+          end
+        end
+
+        unless defined?(BlogPost)
+          class BlogPost < ActiveRecord::Base
+            has_many :blog_post_comments
+            belongs_to :blog_author
+
+            default_scope -> { order(:title) }
+          end
+        end
+
+        unless defined?(BlogPostComment)
+          class BlogPostComment < ActiveRecord::Base
+            belongs_to :blog_post
+
+            default_scope -> { order(created_at: :desc) }
+          end
+        end
+      end
+
+      subject(:ability) { Ability.new(nil) }
+
+      let(:alex) { BlogAuthor.create!(name: "Alex") }
+      let(:josh) { BlogAuthor.create!(name: "Josh") }
+
+      let(:p1) { josh.blog_posts.create!(title: "p1") }
+      let(:p2) { alex.blog_posts.create!(title: "p2") }
+
+      let(:p1c1) { p1.blog_post_comments.create!(body: "p1c1", created_at: Time.new(2019, 8, 25, 1)) }
+      let(:p1c2) { p1.blog_post_comments.create!(body: "p1c2", created_at: Time.new(2019, 8, 25, 2)) }
+
+      let(:p2c1) { p2.blog_post_comments.create!(body: "p2c1", created_at: Time.new(2019, 8, 25, 3)) }
+      let(:p2c2) { p2.blog_post_comments.create!(body: "p2c2", created_at: Time.new(2019, 8, 25, 4)) }
+
+      context 'when default scope sets an order, and abilities dont have extra checks' do
+        before do
+          ability.can :read, BlogPost
+          ability.can :read, BlogPostComment
+        end
+
+        it 'can get accessible records' do
+          accessible = BlogPostComment.accessible_by(ability)
+          expected_bodies_in_order = [p2c2, p2c1, p1c2, p1c1].map(&:body)
+          expect(accessible.map(&:body)).to eq(expected_bodies_in_order)
+        end
+
+        it 'can get accessible records from a has_many' do
+          accessible = p2.blog_post_comments.accessible_by(ability)
+          expected_bodies_in_order = [p2c2, p2c1].map(&:body)
+          expect(accessible.map(&:body)).to eq(expected_bodies_in_order)
+        end
+
+        it 'can get accessible records from a has_many - other post' do
+          accessible = p1.blog_post_comments.accessible_by(ability)
+          expected_bodies_in_order = [p1c2, p1c1].map(&:body)
+          expect(accessible.map(&:body)).to eq(expected_bodies_in_order)
+        end
+
+        it 'can get accessible records from a has_many :through' do
+          accessible = alex.blog_post_comments.accessible_by(ability)
+          expected_bodies_in_order = [p2c2, p2c1].map(&:body)
+          expect(accessible.map(&:body)).to eq(expected_bodies_in_order)
+        end
+
+        it 'can get accessible records from a has_many :through - other user' do
+          accessible = josh.blog_post_comments.accessible_by(ability)
+          expected_bodies_in_order = [p1c2, p1c1].map(&:body)
+          expect(accessible.map(&:body)).to eq(expected_bodies_in_order)
+        end
+      end
+
+      context 'when default scope sets an order, and abilities have extra checks' do
+        before do
+          ability.can :read, BlogPost
+          # this is the only change vs. the above context -- in this context we can *only* see alex's posts
+          ability.can :read, BlogPostComment, blog_post: { blog_author: { id: alex.id } }
+        end
+
+        it 'can get accessible records' do
+          accessible = BlogPostComment.accessible_by(ability)
+          expected_bodies_in_order = [p2c2, p2c1].map(&:body)
+          expect(accessible.map(&:body)).to eq(expected_bodies_in_order)
+        end
+
+        it 'can get accessible records from a has_many' do
+          accessible = p2.blog_post_comments.accessible_by(ability)
+          expected_bodies_in_order = [p2c2, p2c1].map(&:body)
+          expect(accessible.map(&:body)).to eq(expected_bodies_in_order)
+        end
+
+        it 'can get accessible records from a has_many - none returned' do
+          accessible = p1.blog_post_comments.accessible_by(ability)
+          expected_bodies_in_order = []
+          expect(accessible.map(&:body)).to eq(expected_bodies_in_order)
+        end
+
+        it 'can get accessible records from a has_many :through' do
+          accessible = alex.blog_post_comments.accessible_by(ability)
+          expected_bodies_in_order = [p2c2, p2c1].map(&:body)
+          expect(accessible.map(&:body)).to eq(expected_bodies_in_order)
+        end
+
+        it 'can get accessible records from a has_many :through - none returned' do
+          accessible = josh.blog_post_comments.accessible_by(ability)
+          expected_bodies_in_order = []
+          expect(accessible.map(&:body)).to eq(expected_bodies_in_order)
+        end
+      end
+    end
+  end
+end
