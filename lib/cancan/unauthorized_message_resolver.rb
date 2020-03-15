84554d6c110a87df156cff92ca554b257cd9ae39
ayumu mochizuki <ayumu.mochizuki@livesense.co.jp>
2020-03-16 02:28:12 +0900

    Add support to translate action (#594)
    Suport translate action name

--- lib/cancan/unauthorized_message_resolver.rb
+++ lib/cancan/unauthorized_message_resolver.rb
@@ -4,7 +4,8 @@ module CanCan
   module UnauthorizedMessageResolver
     def unauthorized_message(action, subject)
       keys = unauthorized_message_keys(action, subject)
-      variables = { action: action.to_s }
+      variables = {}
+      variables[:action] = I18n.translate("actions.#{action}", default: action.to_s)
       variables[:subject] = translate_subject(subject)
       message = I18n.translate(keys.shift, **variables.merge(scope: :unauthorized, default: keys + ['']))
       message.blank? ? nil : message
