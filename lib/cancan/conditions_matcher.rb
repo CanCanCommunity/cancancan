72cda945f3d42e3ae3e686be841a5f60bde13587
Nick Flueckiger <nick.flueckiger@renuo.ch>
2020-11-05 08:44:30 +0100

    Implement style suggestion

--- lib/cancan/conditions_matcher.rb
+++ lib/cancan/conditions_matcher.rb
@@ -78,8 +78,7 @@ module CanCan
 
     def hash_condition_match?(attribute, value)
       if attribute.is_a?(Array) || (defined?(ActiveRecord) && attribute.is_a?(ActiveRecord::Relation))
-        match_results = attribute.map { |element| matches_conditions_hash?(element, value) }
-        match_results.any?
+        attribute.to_a.any? { |element| matches_conditions_hash?(element, value) }
       else
         attribute && matches_conditions_hash?(attribute, value)
       end
