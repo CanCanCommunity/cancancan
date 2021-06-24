70b983dc763d4c255fb5d2f0277e0aaf30dd63bf
kaspernj <k@spernj.org>
2021-06-24 14:06:24 +0200

    Fixed SQL in spec

--- spec/cancan/model_adapters/has_and_belongs_to_many_spec.rb
+++ spec/cancan/model_adapters/has_and_belongs_to_many_spec.rb
@@ -89,14 +89,14 @@ RSpec.describe CanCan::ModelAdapters::ActiveRecord5Adapter do
           expect(ability.model_adapter(House, :read))
             .to generate_sql("SELECT \"houses\".*
                              FROM \"houses\"
-                             JOIN \"houses\" AS \"houses_alias\" ON \"houses_alias\".\"id\" = \"houses\".\"id\"
+                             INNER JOIN \"houses\" AS \"houses_alias\" ON \"houses_alias\".\"id\" = \"houses\".\"id\"
                              WHERE (EXISTS (SELECT 1
                                FROM \"houses\"
-                               LEFT OUTER JOIN \"houses_people\" ON \"houses_people\".\"house_id\" = \"houses\".\"id\"
-                               LEFT OUTER JOIN \"people\" ON \"people\".\"id\" = \"houses_people\".\"person_id\"
-                               WHERE
-                                 \"people\".\"id\" = #{@person1.id} AND
-                                 (\"houses\".\"id\" = \"houses_alias\".\"id\")))
+                               INNER JOIN \"houses_people\" ON \"houses_people\".\"house_id\" = \"houses\".\"id\"
+                               INNER JOIN \"people\" ON \"people\".\"id\" = \"houses_people\".\"person_id\"
+                               WHERE (\"houses\".\"id\" = \"houses_alias\".\"id\") AND
+                               (\"people\".\"id\" = #{@person1.id})
+                               LIMIT 1))
                              ")
         end
       end
