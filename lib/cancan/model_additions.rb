0dc1d633009563ca004f8d84fbd57dd4dc08e285
Carlos Palhares <chjunior@gmail.com>
2021-07-16 16:45:49 -0300

    Further documentation improvements

--- lib/cancan/model_additions.rb
+++ lib/cancan/model_additions.rb
@@ -26,66 +26,68 @@ module CanCan
         end
       end
 
-      # Provides a scope within the model to find instances of the model that
-      # are accessible by the given ability within the given action/subject
-      # permission pair.
+      # Provides a scope within the model to find instances of the model that are accessible
+      # by the given ability within the given action/subject permission pair.
+      #
       # I.E.:
-      #    Given the scenario below
       #
-      #      class Department < ActiveRecord::Base
-      #      end
+      # Given the scenario below:
       #
-      #      class User < ActiveRecord::Base
-      #        belongs_to :department
-      #      end
+      #   class Department < ActiveRecord::Base
+      #   end
       #
-      #      class Ability
-      #        include CanCan::Ability
+      #   class User < ActiveRecord::Base
+      #     belongs_to :department
+      #   end
       #
-      #        def initialize(user)
-      #          can :contact, User, { department: { id: user.department_id } }
-      #          can :contact, User, { department: { id: user.managing_department_ids } } if user.manager?
-      #        end
-      #      end
+      #   class Ability
+      #     include CanCan::Ability
       #
-      #    This would give you a list of territories that the given ability can
-      #    contact their users:
+      #     def initialize(user)
+      #       can :contact, User, { department: { id: user.department_id } }
+      #       can :contact, User, { department: { id: user.managing_department_ids } } if user.manager?
+      #     end
+      #   end
       #
-      #      > user = User.new(department_id: 13, manager: false)
-      #      > ability = Ability.new(user)
-      #      > Department.accessible_through(ability, :contact, User).to_sql
-      #      => SELECT * FROM territories WHERE id = 13
+      # The following would give you a list of departments that the given ability can contact their users:
       #
-      #      > user = User.new(department_id: 13, managing_department_ids: [2, 3, 4], manager: true)
-      #      > ability = Ability.new(user)
-      #      > Department.accessible_through(ability, :contact, User).to_sql
-      #      => SELECT * FROM territories WHERE ((id = 13) OR (id IN (2, 3, 4)))
+      #   > user = User.new(department_id: 13, manager: false)
+      #   > ability = Ability.new(user)
+      #   > Department.accessible_through(ability, :contact, User).to_sql
+      #   => SELECT * FROM departments WHERE id = 13
+      #   #
+      #   > user = User.new(department_id: 13, managing_department_ids: [2, 3, 4], manager: true)
+      #   > ability = Ability.new(user)
+      #   > Department.accessible_through(ability, :contact, User).to_sql
+      #   => SELECT * FROM departments WHERE ((id = 13) OR (id IN (2, 3, 4)))
       #
-      #    Sometimes the name of the relation doesn't match the model. When that happens, you can override it with `relation`:
+      # Sometimes the name of the relation doesn't match the model. When that happens, you can override it
+      # with `relation`:
       #
-      #      class User < ActiveRecord::Base
-      #        has_many :managing_users, class_name: "User", foreign_key: :managed_by_id
-      #      end
+      #   class User < ActiveRecord::Base
+      #     has_many :managing_users, class_name: "User", foreign_key: :managed_by_id
+      #   end
       #
-      #      class Ability
-      #        include CanCan::Ability
+      #   class Ability
+      #     include CanCan::Ability
       #
-      #        def initialize(user)
-      #          can :contact, User, { managing_users: { id: user.department_id } }
-      #        end
-      #      end
+      #     def initialize(user)
+      #       can :contact, User, { managing_users: { id: user.department_id } }
+      #     end
+      #   end
       #
-      #    The following would give you a list of territories that the given ability can contact their users:
+      # This would give you a list of departments that the given ability can contact their users:
       #
-      #      > user = User.new(department_id: 13, manager: false)
-      #      > ability = Ability.new(user)
-      #      > Department.accessible_through(ability, :contact, User).to_sql
-      #      => SELECT * FROM territories WHERE id = 13
+      #   > user = User.new(department_id: 13, manager: false)
+      #   > ability = Ability.new(user)
+      #   > Department.accessible_through(ability, :contact, User).to_sql
+      #   => SELECT * FROM departments WHERE id = 13
+      #   >
+      #   > user = User.new(department_id: 13, managing_department_ids: [2, 3, 4], manager: true)
+      #   > ability = Ability.new(user)
+      #   > Department.accessible_through(ability, :contact, User).to_sql
+      #   => SELECT * FROM departments WHERE ((id = 13) OR (id IN (2, 3, 4)))
       #
-      #      > user = User.new(department_id: 13, managing_department_ids: [2, 3, 4], manager: true)
-      #      > ability = Ability.new(user)
-      #      > Department.accessible_through(ability, :contact, User).to_sql
-      #      => SELECT * FROM territories WHERE ((id = 13) OR (id IN (2, 3, 4)))
       def accessible_through(ability, action, subject, relation: model_name.element, strategy: CanCan.accessible_by_strategy)
         CanCan.with_accessible_by_strategy(strategy) do
           ability.relation_model_adapter(self, action, subject, relation)
